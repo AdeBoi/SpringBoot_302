b280b3a8dafde5fdb119f980b1cdf26447d0eaa8
adeboi <adebotheentertainer@gmail.com>
2019-04-01 21:15:30 -0400

    2nd version

--- src/main/java/com/xife/springboot_302/HomeController.java
+++ src/main/java/com/xife/springboot_302/HomeController.java
@@ -2,6 +2,7 @@ package com.xife.springboot_302;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
 
+@Controller
 public class HomeController
 {
     @Autowired
